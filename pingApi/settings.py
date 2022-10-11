0498df1e63e838a9df53f02633f6e193d2e4c7cc
maaqib121 <maaqib121@gmail.com>
2022-10-12 02:42:54 +0500

    feat: install and configure django-environ

--- pingApi/settings.py
+++ pingApi/settings.py
@@ -12,9 +12,16 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+import environ
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+# Take environment variables from .env file
+env = environ.Env(DEBUG=(bool, False))
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 
 # Quick-start development settings - unsuitable for production
