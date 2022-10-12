d186e156fbcfc4dc07d524897eaa076c7fef1dde
maaqib121 <maaqib121@gmail.com>
2022-10-13 04:24:25 +0500

    feat: register company model to admin site

--- companies/admin.py
+++ companies/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from companies.models import Company
 
-# Register your models here.
+
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'created_at')
+    list_display_links = ('id', 'name')
+    search_fields = ('name',)
+
+
+admin.site.register(Company, CompanyAdmin)
