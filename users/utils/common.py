2c2f5a78ced53d89d07a01e0176003aecc803db6
maaqib121 <maaqib121@gmail.com>
2022-10-16 23:48:08 +0500

    feat: replace sendgrid with postmark in reset_password_email

--- users/utils/common.py
+++ users/utils/common.py
@@ -35,15 +35,14 @@ def send_reset_password_email(user, redirect_uri):
         'uidb64': urlsafe_base64_encode(force_bytes(user.id)),
         'token': user.confirmation_token,
     })
-    mail = Mail(
-        from_email=settings.SENDGRID_SENDER_EMAIL,
-        to_emails=user.email,
-        subject='Reset Ping-App Password',
-        html_content=message
-    )
     try:
-        send_grid = SendGridAPIClient(settings.SENDGRID_API_KEY)
-        send_grid.send(mail)
+        postmark = PostmarkClient(server_token=settings.POSTMARK_SERVER_TOKEN)
+        postmark.emails.send(
+            From=settings.POSTMARK_SENDER_EMAIL,
+            To=user.email,
+            Subject='Reset Ping-App Password',
+            HtmlBody=message
+        )
     except:
         return Response(
             {'errors': {'non_field_errors': ['Could not send confirmation email to this email address.']}},
