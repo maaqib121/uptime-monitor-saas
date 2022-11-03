be2a0ae4f1da4b5ff0d823550f01d8e4c4a7c482
maaqib121 <maaqib121@gmail.com>
2022-11-03 19:14:30 +0500

    feat: add company update request

--- companies/api/v1/views.py
+++ companies/api/v1/views.py
@@ -7,10 +7,17 @@ from companies.api.v1.serializers import CompanySerializer
 
 
 class CompanyView(APIView):
-    http_method_names = ('get',)
+    http_method_names = ('get', 'patch')
     permission_classes = (IsAuthenticated,)
     authentication_classes = (JWTAuthentication,)
 
     def get(self, request):
-        serializer = CompanySerializer(request.user.company)
+        serializer = CompanySerializer(request.user.company, context={'request': request})
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def patch(self, request):
+        serializer = CompanySerializer(request.user.company, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer = CompanySerializer(serializer.save(), context={'request': request})
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response({'errors': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
