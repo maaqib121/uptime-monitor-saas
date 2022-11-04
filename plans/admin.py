f7150b84e97243fc8c57f6590f2e27806599c8f1
maaqib121 <maaqib121@gmail.com>
2022-11-04 17:22:41 +0500

    feat: add company in plan list_display

--- plans/admin.py
+++ plans/admin.py
@@ -3,7 +3,7 @@ from plans.models import Plan, Price
 
 
 class PlanAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'allowed_users', 'allowed_domains', 'allowed_urls')
+    list_display = ('id', 'title', 'allowed_users', 'allowed_domains', 'allowed_urls', 'company')
     list_display_links = ('id', 'title')
     search_fields = ('title',)
 
