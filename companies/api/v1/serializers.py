1c0611e69dce2c32429b65293cbec534f27bc88a
maaqib121 <maaqib121@gmail.com>
2022-11-04 16:58:58 +0500

    feat: serialize subscribed plan price in company show request

--- companies/api/v1/serializers.py
+++ companies/api/v1/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from rest_framework.fields import empty
 from django.conf import settings
 from companies.models import Company
+from plans.api.v1.serializers import PriceSerializer
 from datetime import datetime, timedelta
 from pytz import timezone
 from math import ceil
@@ -16,6 +17,7 @@ class CompanySerializer(serializers.ModelSerializer):
         super().__init__(instance, data, **kwargs)
         if data == empty:
             self.fields['remaining_trial_days'] = serializers.SerializerMethodField()
+            self.fields['subscribed_plan'] = PriceSerializer()
 
     def get_remaining_trial_days(self, obj):
         return ceil((obj.created_at + timedelta(days=7) - datetime.now(tz=timezone(settings.TIME_ZONE))).total_seconds() / (60 * 60 * 24))
