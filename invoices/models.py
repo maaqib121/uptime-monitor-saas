8ddca77337c16585597f396ea463c264b6569f3b
maaqib121 <maaqib121@gmail.com>
2022-11-05 01:35:13 +0500

    rename: field amount_paid to amount in invoice field

--- invoices/models.py
+++ invoices/models.py
@@ -6,6 +6,6 @@ class Invoice(models.Model):
     stripe_invoice_id = models.CharField(max_length=100)
     company = models.ForeignKey(Company, on_delete=models.CASCADE)
     plan_name = models.CharField(max_length=100)
-    amount_paid = models.DecimalField(max_digits=10, decimal_places=2)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
     paid = models.BooleanField(default=False)
     created_at = models.DateTimeField()
