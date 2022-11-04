578f3a54c9504315ade9933ea390e50cbe3e08ce
maaqib121 <maaqib121@gmail.com>
2022-11-05 01:34:36 +0500

    feat: create product on stripe on plan creation and price on stripe on price creation through django admin

--- plans/admin.py
+++ plans/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
+from django.conf import settings
 from plans.models import Plan, Price
+import stripe
 
 
 class PlanAdmin(admin.ModelAdmin):
@@ -7,12 +9,32 @@ class PlanAdmin(admin.ModelAdmin):
     list_display_links = ('id', 'title')
     search_fields = ('title',)
 
+    def save_model(self, request, obj, form, change):
+        if not change:
+            metadata = {'company_id': obj.company.id} if obj.company else {}
+            stripe.api_key = settings.STRIPE_SECRET_KEY
+            stripe_product = stripe.Product.create(name=obj.title, metadata=metadata)
+            obj.stripe_product_id = stripe_product.id
+        return super().save_model(request, obj, form, change)
+
 
 class PriceAdmin(admin.ModelAdmin):
     list_display = ('id', 'plan', 'frequency', 'amount')
     list_display_links = ('id', 'plan')
     list_filter = ('frequency',)
     search_fields = ('plan', 'amount')
+
+    def save_model(self, request, obj, form, change):
+        if not change:
+            stripe.api_key = settings.STRIPE_SECRET_KEY
+            stripe_price = stripe.Price.create(
+                unit_amount=obj.amount * 100,
+                currency='eur',
+                product=obj.plan.stripe_product_id,
+                recurring={'interval': 'year' if obj.frequency == Price.Frequency.YEARLY else 'month'}
+            )
+            obj.stripe_price_id = stripe_price.id
+        return super().save_model(request, obj, form, change)
 
 
 admin.site.register(Plan, PlanAdmin)
