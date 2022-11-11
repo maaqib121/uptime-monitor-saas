81bfdaa4302a046b4580273c0192efa965b28ec7
maaqib121 <maaqib121@gmail.com>
2022-11-12 00:30:30 +0500

    feat: add pagination and is_default_payment_source in payment_methods index api

--- payment_methods/api/v1/serializers.py
+++ payment_methods/api/v1/serializers.py
@@ -1,14 +1,29 @@
 from rest_framework import serializers
 from rest_framework.fields import empty
 from django.conf import settings
+from datetime import datetime
+from pytz import timezone
 import stripe
 
 
 class PaymentMethodSerializer(serializers.Serializer):
-    payment_method_id = serializers.CharField()
-
     def __init__(self, instance=None, data=empty, **kwargs):
         super().__init__(instance, data, **kwargs)
+        if data == empty:
+            self.fields['id'] = serializers.CharField()
+            self.fields['billing_details'] = serializers.DictField()
+            self.fields['card'] = serializers.DictField()
+            self.fields['created_at'] = serializers.SerializerMethodField()
+            self.fields['type'] = serializers.CharField()
+            self.fields['is_default_payment_method'] = serializers.SerializerMethodField()
+        else:
+            self.fields['payment_method_id'] = serializers.CharField()
+
+    def get_created_at(self, instance):
+        return datetime.utcfromtimestamp(instance.created)
+
+    def get_is_default_payment_method(self, instance):
+        return instance.id == instance.customer.default_source
 
     def validate(self, attrs):
         stripe.api_key = settings.STRIPE_SECRET_KEY
--- payment_methods/api/v1/views.py
+++ payment_methods/api/v1/views.py
@@ -6,24 +6,35 @@ from rest_framework.response import Response
 from django.conf import settings
 from payment_methods.api.v1.serializers import PaymentMethodSerializer
 from users.permissions import IsCurrentUserAdmin
+from pingApi.utils.pagination import CustomPagination
 import stripe
 
 
-class PaymentMethodView(APIView):
+class PaymentMethodView(APIView, CustomPagination):
     http_method_names = ('get', 'post')
     permission_classes = (IsAuthenticated, IsCurrentUserAdmin)
     authentication_classes = (JWTAuthentication,)
 
-    def get(self, request):
+    def get_stripe_payment_methods(self):
         stripe.api_key = settings.STRIPE_SECRET_KEY
-        if request.user.company.stripe_customer_id:
-            stripe_payment_methods = stripe.Customer.list_payment_methods(
-                request.user.company.stripe_customer_id,
-                type='card'
+        if self.request.user.company.stripe_customer_id:
+            return stripe.Customer.list_payment_methods(
+                self.request.user.company.stripe_customer_id,
+                type='card',
+                expand=['data.customer']
             )['data']
-        else:
-            stripe_payment_methods = []
-        return Response(stripe_payment_methods, status=status.HTTP_200_OK)
+        return []
+
+    def get_paginated_response(self):
+        page = self.paginate_queryset(self.get_stripe_payment_methods(), self.request)
+        serializer = PaymentMethodSerializer(page, many=True, context={'request': self.request})
+        return super().get_paginated_response(serializer.data)
+
+    def get(self, request):
+        if 'no_paginate' in self.request.GET:
+            serializer = PaymentMethodSerializer(self.get_stripe_payment_methods(), many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return self.get_paginated_response()
 
     def post(self, request):
         serializer = PaymentMethodSerializer(data=request.data, context={'user': request.user})
