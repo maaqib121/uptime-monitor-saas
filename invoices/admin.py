be2e2f086b186a086d7194442f509c19702001a1
maaqib121 <maaqib121@gmail.com>
2022-11-19 02:40:14 +0500

    feat: update invoice admin site

--- invoices/admin.py
+++ invoices/admin.py
@@ -3,7 +3,7 @@ from invoices.models import Invoice
 
 
 class InvoiceAdmin(admin.ModelAdmin):
-    list_display = ('id', 'company', 'plan_name', 'amount', 'paid', 'created_at')
+    list_display = ('id', 'company', 'plan_name', 'amount', 'paid', 'invoice_url', 'created_at')
     list_display_links = ('id', 'company')
     list_filter = ('company', 'paid')
     search_fields = ('plan', 'company')
