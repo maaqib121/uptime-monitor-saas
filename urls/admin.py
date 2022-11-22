c489b1f8010c2bb51aceeabf038e1f019abb812d
maaqib121 <maaqib121@gmail.com>
2022-11-22 21:45:38 +0500

    feat: add labels in list_display of url admin

--- urls/admin.py
+++ urls/admin.py
@@ -3,7 +3,7 @@ from urls.models import Url, UrlLabel
 
 
 class UrlAdmin(admin.ModelAdmin):
-    list_display = ('id', 'url', 'domain', 'company')
+    list_display = ('id', 'url', 'domain', 'company', 'labels')
     list_display_links = ('id', 'url')
     list_filter = ('domain', 'company')
     search_fields = ('url',)
