6620a6004f39b2ab5564d5a6b7269bee51db760a
maaqib121 <maaqib121@gmail.com>
2022-11-22 07:37:34 +0500

    feat: register domain and domainlabel model to admin

--- domains/admin.py
+++ domains/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from domains.models import Domain, DomainLabel
 
-# Register your models here.
+
+class DomainAdmin(admin.ModelAdmin):
+    list_display = ('id', 'domain_url', 'country', 'company', 'labels')
+    list_display_links = ('id', 'domain_url')
+    list_filter = ('company',)
+    search_fields = ('domain_url',)
+
+    def labels(self, instance):
+        return list(instance.domainlabel_set.values_list('label', flat=True))
+
+
+class DomainLabelAdmin(admin.ModelAdmin):
+    list_display = ('id', 'label', 'domain')
+    list_display_links = ('id', 'label')
+    list_filter = ('domain',)
+    search_fields = ('label',)
+
+
+admin.site.register(Domain, DomainAdmin)
+admin.site.register(DomainLabel, DomainLabelAdmin)
