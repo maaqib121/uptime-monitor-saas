c851e2710c4a3f04606dd7c36e159ef4387a3196
maaqib121 <maaqib121@gmail.com>
2022-11-22 20:51:18 +0500

    feat: register url and urllabel models to admin

--- urls/admin.py
+++ urls/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from urls.models import Url, UrlLabel
 
-# Register your models here.
+
+class UrlAdmin(admin.ModelAdmin):
+    list_display = ('id', 'url', 'domain')
+    list_display_links = ('id', 'url')
+    list_filter = ('domain',)
+    search_fields = ('url',)
+
+    def labels(self, instance):
+        return list(instance.urllabel_set.values_list('label', flat=True))
+
+
+class UrlLabelAdmin(admin.ModelAdmin):
+    list_display = ('id', 'label', 'url')
+    list_display_links = ('id', 'label')
+    list_filter = ('url',)
+    search_fields = ('label',)
+
+
+admin.site.register(Url, UrlAdmin)
+admin.site.register(UrlLabel, UrlLabelAdmin)
