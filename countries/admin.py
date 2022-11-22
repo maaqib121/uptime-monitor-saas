fe905e69733ff6d0c9f9fd727ef77a55ca4b9fa0
maaqib121 <maaqib121@gmail.com>
2022-11-22 06:58:28 +0500

    feat: register country model to admin site

--- countries/admin.py
+++ countries/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from countries.models import Country
 
-# Register your models here.
+
+class CountryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'code', 'continent', 'is_active')
+    list_filter = ('continent', 'is_active')
+    list_display_links = ('id', 'name')
+    ordering = ('id',)
+    search_fields = ('name', 'code')
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+
+admin.site.register(Country, CountryAdmin)
