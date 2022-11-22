b3dceccf3bc50d1492476692ae13a619858bd174
maaqib121 <maaqib121@gmail.com>
2022-11-23 00:00:04 +0500

    feat: add pagination in invoice api

--- invoices/api/v1/views.py
+++ invoices/api/v1/views.py
@@ -1,13 +1,14 @@
 from rest_framework.views import APIView
-from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from invoices.api.v1.serializers import InvoiceSerializer
 from users.permissions import IsCurrentUserAdmin
+from pingApi.utils.pagination import CustomPagination
 
 
-class InvoiceView(APIView):
+class InvoiceView(APIView, CustomPagination):
     http_method_names = ('get',)
     permission_classes = (IsAuthenticated,)
     authentication_classes = (JWTAuthentication, IsCurrentUserAdmin)
@@ -15,6 +16,13 @@ class InvoiceView(APIView):
     def get_queryset(self):
         return self.request.user.company.invoice_set.filter(paid=True)
 
+    def get_paginated_response(self):
+        page = self.paginate_queryset(self.get_queryset(), self.request)
+        serializer = InvoiceSerializer(page, many=True, context={'request': self.request})
+        return super().get_paginated_response(serializer.data)
+
     def get(self, request):
-        serializer = InvoiceSerializer(self.get_queryset(), many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        if 'no_paginate' in request.GET:
+            serializer = InvoiceSerializer(self.get_queryset(), many=True, context={'request': self.request})
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return self.get_paginated_response()
