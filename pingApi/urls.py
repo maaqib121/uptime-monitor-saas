0ab1f10103e4e691923236a91314f7106add1b57
maaqib121 <maaqib121@gmail.com>
2022-12-12 03:01:20 +0500

    refactor: admin site

--- pingApi/urls.py
+++ pingApi/urls.py
@@ -34,3 +34,7 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+admin.site.site_header = 'Takemint Admin'
+admin.site.index_title = 'Admin'
+admin.site.site_title = 'Takemint'
--- ping_results/apps.py
+++ ping_results/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class PingResultsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'ping_results'
+    verbose_name = "ping results"
