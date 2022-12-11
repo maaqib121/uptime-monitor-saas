dee3ea920f29e7172020bc86931b61d993bc0734
maaqib121 <maaqib121@gmail.com>
2022-12-11 05:34:18 +0500

    feat: register pingresult model to admin site

--- ping_results/admin.py
+++ ping_results/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from ping_results.models import PingResult
 
-# Register your models here.
+
+class PingResultAdmin(admin.ModelAdmin):
+    list_display = ('id', 'url', 'domain', 'status_code', 'company', 'created_at')
+    list_display_links = ('url', 'domain', 'company')
+    list_filter = ('url', 'company')
+    search_fields = ('status_code',)
+
+    def domain(self, instance):
+        return instance.domain
+
+
+admin.site.register(PingResult, PingResultAdmin)
