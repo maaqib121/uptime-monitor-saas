b2fcd8071fae2002934eb8269eadaf05e770a055
maaqib121 <maaqib121@gmail.com>
2022-12-11 05:34:00 +0500

    feat: rename field in pingresult model

--- ping_results/models.py
+++ ping_results/models.py
@@ -4,10 +4,13 @@ from urls.models import Url
 
 
 class PingResult(models.Model):
-    response_code = models.PositiveIntegerField()
+    status_code = models.PositiveIntegerField()
     url = models.ForeignKey(Url, on_delete=models.CASCADE)
     company = models.ForeignKey(Company, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.url} ({self.status_code})'
 
     @property
     def domain(self):
