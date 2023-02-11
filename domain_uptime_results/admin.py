d80a6c32a67870d9c3361f8c99f59da42791e8cb
maaqib121 <maaqib121@gmail.com>
2023-02-12 04:33:04 +0500

    feat: register domainuptimeresult model to admin site

--- domain_uptime_results/admin.py
+++ domain_uptime_results/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from domain_uptime_results.models import DomainUptimeResult
 
-# Register your models here.
+
+class DomainUptimeResultAdmin(admin.ModelAdmin):
+    list_display = ('id', 'domain', 'status', 'ssl_validity', 'status_code', 'response_time', 'created_at')
+    list_display_links = ('id', 'domain')
+    list_filter = ('domain', 'company')
+    search_fields = ('status_code',)
+
+    def domain(self, instance):
+        return instance.domain
+
+
+admin.site.register(DomainUptimeResult, DomainUptimeResultAdmin)
