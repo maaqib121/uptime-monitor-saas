b7c0ec6db05aec8defab85e5f3e7cad95445abe8
maaqib121 <maaqib121@gmail.com>
2023-03-01 16:10:01 +0500

    fix: round last_health_score to 2 decimal points in company serializer

--- companies/api/v1/serializers.py
+++ companies/api/v1/serializers.py
@@ -37,7 +37,7 @@ class CompanySerializer(serializers.ModelSerializer):
             'total_users': User.objects.filter(profile__company=instance).count(),
             'total_domains': instance.domain_set.count(),
             'total_urls': instance.url_set.count(),
-            'last_health_score': instance.url_set.filter(last_ping_status_code=200).count() / instance.url_set.count() * 100
+            'last_health_score': round(instance.url_set.filter(last_ping_status_code=200).count() / instance.url_set.count() * 100, 2)
         }
 
 
