9a040c18ac26ab06f4ad5df0bd3b84195285beee
maaqib121 <maaqib121@gmail.com>
2023-07-20 18:08:47 +0500

    feat: remove fields from company serializer

--- companies/api/v1/serializers.py
+++ companies/api/v1/serializers.py
@@ -10,29 +10,19 @@ import requests
 class CompanySerializer(serializers.ModelSerializer):
     class Meta:
         model = Company
-        exclude = ('stripe_customer_id', 'stripe_subscription_id', 'google_refresh_token')
+        exclude = ('stripe_customer_id', 'google_refresh_token')
 
     def __init__(self, instance=None, data=empty, **kwargs):
         super().__init__(instance, data, **kwargs)
         if data == empty:
             self.fields['statistics'] = serializers.SerializerMethodField()
             self.fields['remaining_trial_days'] = serializers.SerializerMethodField()
-            self.fields['plan_restrictions'] = serializers.SerializerMethodField()
-            self.fields['subscribed_plan'] = PriceSerializer()
             if 'no_created_by' not in self.context:
                 from users.api.v1.serializers import UserSerializer
                 self.fields['created_by'] = UserSerializer()
 
     def get_remaining_trial_days(self, instance):
         return instance.remaining_trail_days
-
-    def get_plan_restrictions(self, instance):
-        return {
-            'allowed_users': instance.allowed_users,
-            'allowed_domains': instance.allowed_domains,
-            'allowed_urls': instance.allowed_urls,
-            'ping_interval': instance.ping_interval
-        }
 
     def get_statistics(self, instance):
         total_urls = instance.url_set.count()
