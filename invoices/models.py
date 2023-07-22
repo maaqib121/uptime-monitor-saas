59be0e63b4577d31868baa69ce6a23e1e50baf8a
maaqib121 <maaqib121@gmail.com>
2023-07-22 17:41:35 +0500

    feat: add domain ref in invoice model

--- invoices/models.py
+++ invoices/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from companies.models import Company
+from domains.models import Domain
 
 
 class Invoice(models.Model):
     stripe_invoice_id = models.CharField(max_length=100)
     company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    domain = models.ForeignKey(Domain, on_delete=models.CASCADE)
     plan_name = models.CharField(max_length=100)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     paid = models.BooleanField(default=False)
